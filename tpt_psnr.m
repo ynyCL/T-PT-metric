5b219044ac079a85e3ddff0a42cd0b65bfef9645
lincolnBush <ynylincolncam@gmail.com>
2018-10-01 14:13:49 +0100

    update

--- tpt_psnr.m
+++ tpt_psnr.m
@@ -4,37 +4,23 @@ function Q = tpt_psnr(I1, I2, param)
 % I2: test image
 % param：input parameters
 % This is the code from the paper: Nanyang Ye, Maria Perez-Ortiz and Rafal Mantiuk. Trained Perceptual Transform For Quality Assessment of High Dynamic Range Images and Video
-if max(I1(:))< 1
+
+if max(I1(:))<= 1
     warning('Luminance very low, please check whether use the correct luminance')
 end
 
-if ~exist('param')
-    disp('using default value')
-    params.m = 78.8438;
-    params.n = 0.1593;
-    params.c1 = 0.14249;
-    params.c2 = 2.192;
-    params.c3 = 0.30499;
-else
-    if isstruct(param)
-        params = param;
-    else
-        params.m = param(1);
-        params.n = param(2);
-        params.c1 = param(3);
-        params.c2 = param(4);
-        params.c3 = param(5);
-    end
-end
+pars(1) = 0.14249;
+pars(2) = 2.192;
+pars(3) = 0.30499;
 
-p_peak = pq2_encode(1e4, params);
+p_peak = pu2_encode_par(1e4, pars);
 
-P1 = pq2_encode(get_luminance(I1), params);
-P2 = pq2_encode(get_luminance(I2), params);
+P1 = pu2_encode_par(get_luminance(I1), pars);
+P2 = pu2_encode_par(get_luminance(I2), pars);
 
 MSE = sum((P1(:)-P2(:)).^2)./numel(P1);
 Q = 20 * log10( p_peak / sqrt(MSE) );
-Q = double(Q);
+
 end
 
 function Y = get_luminance( img )
@@ -49,21 +35,5 @@ elseif( dims == 1 || dims == 2 )
 else
     error( 'get_luminance: wrong matrix dimension' );
 end
-
-end
-
-function V = pq2_encode(l, params)
-L = 10000;
-m = params.m;
-n = params.n;
-c1 = params.c1;
-c2 = params.c2;
-c3 = params.c3;
-
-
-hdrin = double(l)./L;
-alpha = hdrin.^n;
-
-V = ( (alpha.*c2 + c1)./(1+alpha.*c3) ).^m;
 
 end
--- tpt_ssim.m
+++ tpt_ssim.m
@@ -1,39 +1,26 @@
-function Q = tpt_ssim( I1, I2 , param)
+function Q = tpt_ssim( I1, I2, param)
 % argument of the function
 % I1: reference image
 % I2: test image
 % param：input parameters
 % This is the code from the paper: Nanyang Ye, Maria Perez-Ortiz and Rafal Mantiuk. Trained Perceptual Transform For Quality Assessment of High Dynamic Range Images and Video
-
-
-if max(I1(:))< 1
+ 
+if max(I1(:))<= 1
     warning('Luminance very low, please check whether use the correct luminance')
 end
 
-if ~exist('params')
-    params.m = 78.8438;
-    params.n = 0.1593;
-    params.c1 = 0.14249;
-    params.c2 = 2.192;
-    params.c3 = 0.30499;
-else
-    params.m = param(1);
-    params.n = param(2);
-    params.c1 = param(3);
-    params.c2 = param(4);
-    params.c3 = param(5);
-end
+pars(1) = 0.14249;
+pars(2) = 2.192;
+pars(3) = 0.30499;
 
-  luma1 = pq2_encode(get_luminance(I1), params)*255;
-  luma2 = pq2_encode(get_luminance(I2), params)*255;
+luma1 =  pu2_encode_par(get_luminance(I1), pars);
+luma2 =  pu2_encode_par(get_luminance(I2), pars);
 
 K(1) = 0.01;
 K(2) = 0.03;
-% Q = qm_ssim(luma1, luma2, K, fspecial('gaussian', 11, 1.5), pu2_encode(1e4));
-Q = qm_ssim(luma1, luma2, K, fspecial('gaussian', 11, 1.5), pq2_encode(1e4, params));
+Q = qm_ssim(luma1, luma2, K, fspecial('gaussian', 11, 1.5), pu2_encode_par(1e4, pars));
+
 end
-
-
 
 function Y = get_luminance( img )
 % Return 2D matrix of luminance values for 3D matrix with an RGB image
